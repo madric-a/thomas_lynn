f84876a84d7f35498ee5b771e8b68adb92df4123
Madric A <110039061+madric-a@users.noreply.github.com>
2023-03-29 16:58:34 +0700

    Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Thomas Tin Sein</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D games, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I develop 3D games, <br className='sm:block hidden' />
+            and deploy web applications.
           </p>
         </div>
       </div>
